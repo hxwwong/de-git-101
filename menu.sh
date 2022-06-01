64db80de157453dc3cbbd0c3ad5c38a75175d98e
hxwwong <hxwwong@gmail.com>
2022-06-01 15:11:30 +0800

    added a line break to menu.sh user input

--- menu.sh
+++ menu.sh
@@ -3,7 +3,8 @@
 echo "1. Show disk usage." 
 echo "2. Show system uptime."
 echo "3. Show the users logged into the system."
-read -p "What would you like to do? (Enter q to quit.)" action 
+echo "What would you like to do? (Enter q to quit.)" 
+read action
 
 case $action in
     1) du ;; 
